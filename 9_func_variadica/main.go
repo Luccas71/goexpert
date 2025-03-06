a6aa4e1c1b84bebc68f68615d7b4a75788e2b6df
Lucas Volpato <lmaciel916@gmail.com>
2025-03-06 15:27:08 +0000

    feat: anotações

--- 9_func_variadica/main.go
+++ 9_func_variadica/main.go
@@ -18,3 +18,16 @@ func sum(numeros ...int) int {
 	função variádica pode receber varios argumentos desde que sejam do type permitido pela função
 	(numeros ...int)
 */
+
+/*
+=> Parâmetro
+	É definido na declaração da função
+	Indica os dados que a função espera receber
+	Define o tipo e o número de entradas que uma função pode aceitar
+	É utilizado quando está sendo criado um método
+=> Argumento
+	É o valor real que é passado para a função ou método quando ele é chamado
+	É utilizado para executar métodos que esteja parametrizado
+	Tem que ser do mesmo tipo que foi utilizado no parâmetro
+
+*/
